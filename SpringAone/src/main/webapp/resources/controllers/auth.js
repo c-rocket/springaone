ac31d8ad26d167340c9e9aaecaae520fb277a457
ccrock <cam.crockett.12@gmail.com>
2016-03-31 14:18:20 -0600

    Initial Commit

---
+++ SpringAone/src/main/webapp/resources/controllers/auth.js
@@ -0,0 +1,81 @@
+'use strict'
+
+app.controller('AuthController', function($q, $scope, $rootScope, $location, toaster, User, Auth) {
+
+	$scope.login = function(user) {
+
+		Auth.login.get({
+			email : user.email,
+			pw : user.password
+		}, function(resp) {
+			if (resp.USER_ID != null) {
+				user.uid = resp.USER_ID
+				user.name = resp.USER_NAME
+				user.gravatar = resp.USER_GRAVATAR
+				user.email = resp.USER_EMAIL
+				user.signedIn = true
+				User.setCurrentUser(user)
+				if (User.isSignedIn) {
+					var currentuser = User.getCurrentUser()
+
+					$rootScope.$broadcast('userEvent', user)
+					toaster.pop('success', "Logged in!")
+					$location.path(baseUrl + '/browse')
+				} else {
+
+					toaster.pop('error', "Login failed!")
+					$scope.user = ''
+				}
+			}else {
+				toaster.pop('error', "Login failed! Check Credentials")
+			}
+		});
+
+	}; // ----end $scope.login
+	$scope.register = function(user) {
+
+		var newUser = {
+			username : user.name,
+			email : user.email,
+			pw : user.password
+		}
+		User.registerUser(newUser).then(function(user) {
+			if (user.data) {
+				user.uid = user.data.USER_ID
+				user.name = user.data.USER_NAME
+				user.gravatar = user.data.USER_GRAVATAR
+				user.email = user.data.USER_EMAIL
+				user.signedIn = true
+				User.setCurrentUser(user)
+				if (User.isSignedIn) {
+					var currentuser = User.getCurrentUser()
+					$rootScope.$broadcast('userEvent', user)
+					toaster.pop('success', "Registered!")
+					$location.path(baseUrl + '/browse')
+				} else {
+					toaster.pop('error', "Signup failed!")
+					$scope.user = ''
+				}
+			}
+		});
+
+	} // ------end $scope.register
+
+	$scope.changePassword = function(user) {
+		var payload = {
+			oldpw : user.oldPass,
+			newpw : user.newPass,
+			email : user.email
+		}
+		User.changePassword(payload).success(function(status) {
+			if (status == 401) {
+				toaster.pop('failed', 'Password change failed! Retry')
+			}else{
+				toaster.pop('success', 'Password changed!')
+				$scope.user = '';
+			}
+		})
+
+	}
+
+});
---
+++ SpringAone/src/main/webapp/resources/services/auth.js
@@ -0,0 +1,59 @@
+'use strict';
+
+app.factory('Auth', [ '$resource', function($resource) {
+
+	return {
+		newUser : $resource(baseUrl + '/user', {}, {
+			method : 'POST'
+		}),
+
+		login : $resource(baseUrl + '/login/:email/:pw', {}, {
+			method : 'GET',
+			isArray : false
+		}),
+
+	};
+} ]);
+
+app.factory('User', function($http) {
+
+	var currentUser = {};
+
+	var User = {
+
+		getCurrentUser : function() {
+
+			return currentUser;
+		},
+
+		registerUser : function(user) {
+
+			return $http.post(baseUrl + '/user', user)
+
+		},
+
+		setCurrentUser : function(user) {
+
+			currentUser.name = user.name;
+			currentUser.gravatar = user.gravatar
+			currentUser.uid = user.uid
+			currentUser.email = user.email
+			currentUser.signedIn = user.signedIn
+
+		},
+
+		isSignedIn : function() {
+			if (currentUser.signedIn) {
+				return true
+			}
+			return false;
+		},
+
+		changePassword : function(payload) {
+			return $http.post(baseUrl + '/userpass', payload)
+		}
+
+	}
+
+	return User;
+});
